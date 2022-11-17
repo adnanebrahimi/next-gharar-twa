932e960ddfff5c5cca19c197df9cb32c75690bd9
Adnan Ebrahimi <adnane.ebrahimi@gmail.com>
2022-11-17 16:43:52 +0330

    use client component in page of video

--- app/video/page.tsx
+++ app/video/page.tsx
@@ -1,12 +1,12 @@
+"use client"
+import {useSearchParams} from "next/navigation";
 import Gharar from "../../components/gharar/gharar.component";
 
-export default function VideoPage({
-  searchParams,
-}: {
-  searchParams: { roomAddress: string };
-}) {
-  return searchParams.roomAddress ? (
-    <Gharar redirectTo={"/"} roomAddress={searchParams.roomAddress}></Gharar>
+export default function VideoPage() {
+  const searchParams = useSearchParams();
+  const roomAddress = searchParams.get('roomAddress');
+  return roomAddress ? (
+    <Gharar redirectTo={"/"} roomAddress={roomAddress}></Gharar>
   ) : (
     <div className="h-screen w-screen flex items-center justify-center">
       <h1>No roomAddress detected in Qeury Params</h1>
