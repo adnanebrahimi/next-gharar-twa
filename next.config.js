7ef04506825adb8ffbece4e46d9c31d7e37c3d21
Adnan Ebrahimi <adnane.ebrahimi@gmail.com>
2022-11-19 12:07:24 +0330

    update next config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require("next-pwa")({
-  dest: "public",
-  disable: process.env.NODE_ENV === "development",
-});
+const withPWA = require("next-pwa");
 
 module.exports = withPWA({
-  // config
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
+  },
   reactStrictMode: true,
   swcMinify: true,
   experimental: {
