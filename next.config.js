12b006fa51514aae048d272b298b101cd6bc2720
Adnan Ebrahimi <adnane.ebrahimi@gmail.com>
2022-11-19 12:29:58 +0330

    pwa

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const runtimeCaching = require("next-pwa/cache");
 const withPWA = require("next-pwa")({
-  dest: "public",
-  disable: process.env.NODE_ENV === "development"
+  dest: "public"
 });
 
 module.exports = withPWA({
