ddb49c19ca0d3478a45a557c2a365d471c7a7863
Adnan Ebrahimi <adnane.ebrahimi@gmail.com>
2022-11-19 10:29:35 +0330

    enable pwa

--- next.config.js
+++ next.config.js
@@ -1,10 +1,18 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   experimental: {
-    appDir: true
-  }
-}
+    appDir: true,
+  },
+};
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const withPWA = require("next-pwa")({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+});
+
+module.exports = withPWA({
+  // config
+  nextConfig,
+});
